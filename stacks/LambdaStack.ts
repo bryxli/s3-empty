4d9487032c8ce1c4a0a34c48d84243c02cadfa4c
bryxli <bryanzero765@gmail.com>
2024-06-19 16:28:36 -0500

    create lambda function and event rule

--- stacks/LambdaStack.ts
+++ stacks/LambdaStack.ts
@@ -1,5 +1,18 @@
-import { StackContext } from "sst/constructs";
+import { Function, StackContext } from "sst/constructs";
+import * as cdk from "aws-cdk-lib";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
 
 export function LambdaStack({ stack }: StackContext) {
+  const emptyFunction = new Function(stack, "function-empty", {
+    handler: "packages/functions/src/main.handler",
+    runtime: "python3.9",
+    memorySize: 1024,
+    timeout: "5 minutes",
+    architecture: "x86_64"
+  });
 
+  new events.Rule(stack, "function-empty-rule", {
+    schedule: events.Schedule.rate(cdk.Duration.days(1)),
+  }).addTarget(new targets.LambdaFunction(emptyFunction));
 }
